b156552f303b7b31665583c16abbf8a04a814ea4
Darius Bacon <darius@wry.me>
2012-11-15 20:32:21 -0500

    new curried Grammar interface. check for multiply-defined rules. add tests.

--- peg.py
+++ peg.py
@@ -3,26 +3,39 @@ Parse a string representation of a grammar.
 """
 
 from re import escape
-from parson import Unparsable, Peg, delay, \
-    alt, catch, chop, hug, empty, nest, seq, \
-    invert, maybe, plus, star
+from parson import *
 
-class Grammar(object):
-    def __init__(self, string, **actions):
-        self.__dict__.update(parse_grammar(string, **actions))
+class Struct(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
 
-def parse_grammar(string, **actions):
+def Grammar(string):
+    rules, items = parse_grammar(string)
+    def bind(**actions):
+        for rule, f in items:
+            rules[rule] = f(actions)
+        return Struct(**rules)
+    return bind
 
+def parse_grammar(string):
+
+    rules = {}
     refs = set()
+
+    # In "lambda a: ..." a is short for actions.
 
     def mk_rule_ref(name):
         refs.add(name)
-        return delay(lambda: rules[name])
+        ref = delay(lambda: rules[name])
+        return lambda a: ref
 
-    mk_chop      = lambda name: chop(actions[name])
-    mk_empty     = lambda: empty
-    mk_literal   = lambda *cs: Peg(escape(''.join(cs)))
-    mk_match     = lambda *cs: Peg(''.join(cs))
+    def lift(peg_op):
+        return lambda *lifted: lambda a: peg_op(*[f(a) for f in lifted])
+
+    mk_chop      = lambda name: lambda a: chop(a[name])
+
+    mk_literal   = lambda *cs: lambda a: Peg(escape(''.join(cs)))
+    mk_match     = lambda *cs: lambda a: Peg(''.join(cs))
 
     _              = Peg(r'(?:\s|#[^\n]*\n?)*')
     name           = Peg(r'([A-Za-z_]\w*)') +_
@@ -31,38 +44,57 @@ def parse_grammar(string, **actions):
     quoted_char    = Peg(r'\\(.)') | r"([^'])"
 
     peg            = delay(lambda: 
-                     term + r'\|' +_+ peg                  >> alt
+                     term + r'\|' +_+ peg               >> lift(alt)
                    | term
-                   | empty                                 >> mk_empty)
+                   | empty                              >> lift(lambda: empty))
 
     term           = delay(lambda:
-                     factor + term                         >> seq
+                     factor + term                      >> lift(seq)
                    | factor)
 
     factor         = delay(lambda:
-                     '!' +_+ factor                        >> invert
-                   | primary + r'\*' +_                    >> star
-                   | primary + r'\+' +_                    >> plus
-                   | primary + r'\?' +_                    >> maybe
+                     '!' +_+ factor                     >> lift(invert)
+                   | primary + r'\*' +_                 >> lift(star)
+                   | primary + r'\+' +_                 >> lift(plus)
+                   | primary + r'\?' +_                 >> lift(maybe)
                    | primary)
 
     primary        = (r'\(' +_+ peg + r'\)' +_
-                   | '{' +_+ peg + '}' +_                  >> catch
-                   | "'" + quoted_char.star() + "'" +_     >> mk_literal
-                   | '/' + regex_char.star() + '/' +_      >> mk_match
-                   | ':' +_+ name                          >> mk_chop
-                   | name                                  >> mk_rule_ref)
+                   | '{' +_+ peg + '}' +_               >> lift(catch)
+                   | "'" + quoted_char.star() + "'" +_  >> mk_literal
+                   | '/' + regex_char.star() + '/' +_   >> mk_match
+                   | ':' +_+ name                       >> mk_chop
+                   | name                               >> mk_rule_ref)
 
-    rule           = name + '=' +_+ (peg>>nest) + r'\.' +_ >> hug
+    rule           = name + '=' +_+ (peg>>lift(nest)) + r'\.' +_ >> hug
     grammar        = _+ rule.plus() + '$'
 
-    rules = dict(grammar(string))
-    undefined = sorted(refs - set(rules))
+    items = grammar(string)
+    lhses = [L for L, R in items]
+    undefined = sorted(refs - set(lhses))
     if undefined: raise Exception("Undefined rules: %s" % ', '.join(undefined))
-    return rules
+    dups = sorted(L for L in set(lhses) if 1 != lhses.count(L))
+    if dups: raise Exception("Multiply-defined rules: %s" % ', '.join(dups))
+    return rules, items
 
 
 # Smoke test
+
+## bad1 = Grammar(r"a = . b = a. a = .")()
+#. Traceback (most recent call last):
+#.   File "peg.py", line 13, in Grammar
+#.     rules, items = parse_grammar(string)
+#.   File "peg.py", line 77, in parse_grammar
+#.     if dups: raise Exception("Multiply-defined rules: %s" % ', '.join(dups))
+#. Exception: Multiply-defined rules: a
+
+## bad2 = Grammar(r"a = b|c|d. c = .")()
+#. Traceback (most recent call last):
+#.   File "peg.py", line 13, in Grammar
+#.     rules, items = parse_grammar(string)
+#.   File "peg.py", line 75, in parse_grammar
+#.     if undefined: raise Exception("Undefined rules: %s" % ', '.join(undefined))
+#. Exception: Undefined rules: b, d
 
 nums = Grammar(r"""
 # This is a comment.
@@ -73,8 +105,7 @@ nums = num ',' nums
      | .
 
 num  = /([0-9]+)/ :int.
-""",
-               int=int)
+""")(int=int)
 sum_nums = lambda s: sum(nums.main(s))
 
 ## sum_nums('10,30,43')
@@ -89,3 +120,15 @@ sum_nums = lambda s: sum(nums.main(s))
 ## nums.main('10,30,43')
 #. (10, 30, 43)
 ## nums.main.match('10,30,43 xxx')
+
+
+# gsub = lambda text, replacement: ''.join(Parser(r"""
+gsub_grammar = Grammar(r"""
+gsub = p gsub | /(.)/ gsub | .
+p = /WHEE/ :replace.
+""")
+def gsub(text, replacement):
+    g = gsub_grammar(replace=lambda: replacement)
+    return ''.join(g.gsub(text))
+## gsub('hi there WHEEWHEE to you WHEEEE', 'GLARG')
+#. 'hi there GLARGGLARG to you GLARGEE'
