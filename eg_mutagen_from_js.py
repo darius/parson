87740bd367a434b56b996267f667842967bf0a32
Darius Bacon <darius@wry.me>
2012-11-29 02:19:16 -0500

    leave out some unneeded parens

--- eg_mutagen_from_js.py
+++ eg_mutagen_from_js.py
@@ -58,7 +58,7 @@ anyone = /./ | /\n/.   # Ugh.
 
      mk_choice   = lambda *xs: ' / '.join(xs),
      mk_fixed    = lambda tag, choice: '%s{ %s }' % (tag, choice),
-     mk_sequence = lambda *xs: ' '.join('(%s)' % x if ' ' in x else x
+     mk_sequence = lambda *xs: ' '.join('(%s)' % x if ' / ' in x else x
                                         for x in xs),
      mk_shuffle  = lambda *xs: '{ %s }' % (' / '.join(xs)),
      mk_weighted = lambda *spairs: ' / '.join('[%s] %s' % (w, '(%s)' % x if ' / ' in x else x)
@@ -91,7 +91,7 @@ def parse_camel(s):
 
 ## goreyfate = open('mutagen/goreyfate.js').read()
 ## translate(goreyfate)
-#. -gorey-fate- = [2] -person-description- -action- -time- / [2] -time- -time-comma- -person-description- -action- / [1] (it was) -time- that -person-description- -action-
+#. -gorey-fate- = [2] -person-description- -action- -time- / [2] -time- -time-comma- -person-description- -action- / [1] it was -time- that -person-description- -action-
 #. -action- = -passive-action- / -active-action-
 #. -active-action- = -active-action-word- -active-action-prep- -a-an- ([1] -target-air- / [2] ()) ([1] -target-age- / [2] ()) -active-action-target-
 #. -target-age- = old / moldering / aged / antiquated
@@ -104,7 +104,7 @@ def parse_camel(s):
 #. -active-action-prep-hi- = down from / off / from
 #. -active-action-word- = fell / tumbled / disappeared / plummeted / vanished / dropped
 #. -passive-action- = -passive-action-word- ([2] -passive-action-qualifier- / [3] ())
-#. -passive-action-qualifier- = away / at sea / without a trace / unexpectedly / mysteriously / into -action-result- / (away into) -action-result-
+#. -passive-action-qualifier- = away / at sea / without a trace / unexpectedly / mysteriously / into -action-result- / away into -action-result-
 #. -action-result- = -dest-noun- / -dest-modifier- -dest-noun- / -a-an- -dest-noun- / -a-an- -dest-modifier- -dest-noun- / -a-an- -dest-form- of -dest-noun- / -a-an- -dest-form- of -dest-modifier- -dest-noun- / -a-an- -dest-modifier- -dest-form- of -dest-noun-
 #. -dest-form- = solidity{ puddle / bucket / vat / heap / cloud / waft }
 #. -dest-modifier- = noisome / pearlescent / foul / fetid / glittering / dark / briny / glistening / cloying
@@ -142,21 +142,21 @@ def parse_camel(s):
 #. -drinking-sentence- = -drinking-sub-sentence- ([1] () / [1] -action-gloss-)
 #. -drinking-sub-sentence- = -people- -drink-action- / -people- -side-action- and -drink-action- / -people- -side-action- , -side-action- , and -drink-action-
 #. -river-march-sentence- = -people- -march-verb- (along / up / down) -river-road- ([1] () / [1] -action-gloss-) / (along / up / down) -river-road- ([1] () / [1] -locale-gloss-) -march-verb- -people- ([1] () / [1] -action-gloss-)
-#. -action-gloss- = , ((spreading / sowing / leaving) (chaos / disaster / wrack and disaster) (in their wake) / (turning / transfiguring / transforming) (the world / all around / everything) into -a-an- (circus / carnival) (() / () / of their own making / of their own design) / (undertaking / preparing / investing) (their best) (effort / determination / and their most impassioned / () / ()) / (as if) ((the world were their) -bivalve- / nothing could be finer / the world had been made for them / they had been created for this day) / (at almost / almost at / at nearly / nearly at / approaching / having achieved) (the end of) -a-journey- / (making the best of) -a-journey- / (refusing / denying / disavowing / repudiating) (any / all / the slightest) (thought of consequence) (() / or causality) / making -a-an- (() / () / holy / unholy / disreputable) (spectacle / display / curiosity) (of themselves) (() / and their cause / and everyone around them)) ,
+#. -action-gloss- = , ((spreading / sowing / leaving) (chaos / disaster / wrack and disaster) in their wake / (turning / transfiguring / transforming) (the world / all around / everything) into -a-an- (circus / carnival) (() / () / of their own making / of their own design) / (undertaking / preparing / investing) their best (effort / determination / and their most impassioned / () / ()) / as if (the world were their -bivalve- / nothing could be finer / the world had been made for them / they had been created for this day) / (at almost / almost at / at nearly / nearly at / approaching / having achieved) the end of -a-journey- / making the best of -a-journey- / (refusing / denying / disavowing / repudiating) (any / all / the slightest) thought of consequence (() / or causality) / making -a-an- (() / () / holy / unholy / disreputable) (spectacle / display / curiosity) of themselves (() / and their cause / and everyone around them)) ,
 #. -side-action- = -side-verb- / -side-verb- / -side-verb- / -side-verb- with (great / some / () / () / ()) (vigor / vim / energy / gusto / enthusiasm) / -tone-adv- -side-verb- / -side-verb- -tone-adv- / -side-verb- (most / quite) -tone-adv-
 #. -side-verb- = { stood / congregated / argued / fought / expostulated / shoved / prayed / sang / danced / lectured / remonstrated / pounded the table / banged their mugs / insulted each other / told stories / remembered old times / demanded satisfaction }
-#. -drink-action- = -drank- -whiskey- / (drank / imbibed) prodigiously / -drank- (prodigious quantities of) -whiskey-
+#. -drink-action- = -drank- -whiskey- / (drank / imbibed) prodigiously / -drank- prodigious quantities of -whiskey-
 #. -drank- = drank / consumed / imbibed / partook of
 #. -whiskey- = their whiskey / whiskey / poteen / moonshine / spirits / alcoholic spirits / strong drink / liquor / strong liquor
-#. -a-journey- = a journey / a difficult journey / a long and difficult journey / their travels / their travails / (a / a sort of) journey (I need not describe / that need hardly be described) / (a / the sort of) (journey sure to be immortalized) (() / () / in legend / in history / in song)
+#. -a-journey- = a journey / a difficult journey / a long and difficult journey / their travels / their travails / (a / a sort of) journey (I need not describe / that need hardly be described) / (a / the sort of) journey sure to be immortalized (() / () / in legend / in history / in song)
 #. -bivalve- = oyster / oyster / oyster / clam / cockle (or mussel, or other bivalve)
-#. -locale-gloss- = , ((a place hitherto) (not troubled by such / untroubled by such / not so troubled) / (far / away / far away) (from the) (travails / clamour / bustle) (of home) / (below the) (() / green / high / high green) (hills / foothills / bluffs) / -a-an- (peaceable / quiet / peaceable and quiet) country (() / betimes / in better times) / (above the) (grimy / slimy / decaying / weed-wracked / chilly north) harbor / (round the bend / past stone / through heather) and (straightaway / off away / up the field / over bridge)) ,
-#. -river-road- = the river / the road / the river road / (the river) -river-name- / the -river-name- river / the -place-name- road / (the road) (from / to) -place-name- / into -place-name-
+#. -locale-gloss- = , (a place hitherto (not troubled by such / untroubled by such / not so troubled) / (far / away / far away) from the (travails / clamour / bustle) of home / below the (() / green / high / high green) (hills / foothills / bluffs) / -a-an- (peaceable / quiet / peaceable and quiet) country (() / betimes / in better times) / above the (grimy / slimy / decaying / weed-wracked / chilly north) harbor / (round the bend / past stone / through heather) and (straightaway / off away / up the field / over bridge)) ,
+#. -river-road- = the river / the road / the river road / the river -river-name- / the -river-name- river / the -place-name- road / the road (from / to) -place-name- / into -place-name-
 #. -river-name- = Arney / Bann / Boyne / Boyle / Blackwater / Clare / Dargyle / Derry / Erne / Fergus / Liffey / Mourne / Nore / Shannon / Tweed / Tyne / Wye / Avon / Tay
 #. -place-name- = Derby / Cork / Mayo / Dublin / Carlow / Kerry / Clare / Wexford / Leith / Lyke / Fife / Falkirk
 #. -march-verb- = marched / went marching / came marching / paraded / walked / came walking / proceeded
 #. -people- = -people-phrase- ([1] () / [1] , -tone-adj- ,) ([1] () / [1] , -tone-adj- and -tone-adj- ,) ([3] () / [1] -people-dash-gloss-)
-#. -people-dash-gloss- = -- ((masters / lords and masters / lords) (of all they) (surveyed / might survey) / (monstrous / exorbitant) in (their / every / their every / face and) aspect / -willing- (to take on all) (comers / mankind / who came before them / who stood before them) / (affronting / an affront to / offending / an offense to / a challenge to) (God and all mankind) / -willing- to (storm / assault) the (gates / armies / will / decree / will and decree) of (Heaven / paradise) / (motivated by the) (best and worst / best / worst) (in the hearts of) (mankind / humanity / the human race) / -unstoppable- and (entirely / wholly / () / ()) -unstoppable- / as (unlikely / absurd / preposterous / laughable) -a-an- -group-word- of (lunatics / reprobates / Bedlamites) as (you're ever like to see / you'd ever want to see / you could find / one might find)) --
+#. -people-dash-gloss- = -- ((masters / lords and masters / lords) of all they (surveyed / might survey) / (monstrous / exorbitant) in (their / every / their every / face and) aspect / -willing- to take on all (comers / mankind / who came before them / who stood before them) / (affronting / an affront to / offending / an offense to / a challenge to) God and all mankind / -willing- to (storm / assault) the (gates / armies / will / decree / will and decree) of (Heaven / paradise) / motivated by the (best and worst / best / worst) in the hearts of (mankind / humanity / the human race) / -unstoppable- and (entirely / wholly / () / ()) -unstoppable- / as (unlikely / absurd / preposterous / laughable) -a-an- -group-word- of (lunatics / reprobates / Bedlamites) as (you're ever like to see / you'd ever want to see / you could find / one might find)) --
 #. -unstoppable- = { unstoppable / uncontainable / unquenchable / irrepressible / insuppressible }
 #. -willing- = willing and able / ready / ready and able / prepared
 #. -tone-adv- = { energetically / enthustiastically / vigorously / noisily / uproariously / stridently / vociferously / riotously / rancorously / clamorously / obstreperously / boisterously / defiantly }
