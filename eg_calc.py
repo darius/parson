80409268f97ea019428c71818aec92833a3c3ca6
Darius Bacon <darius@wry.me>
2012-11-30 09:55:48 -0500

    truediv vs. int div

--- eg_calc.py
+++ eg_calc.py
@@ -8,11 +8,12 @@ from parson import Grammar
 g = Grammar(r"""
 top  = _ exp0 ~/./.
 
-exp0 = exp1 (  '+'_ exp1 :add
-             | '-'_ exp1 :sub)*.
-exp1 = exp2 (  '*'_ exp2 :mul
-             | '/'_ exp2 :div
-             | '%'_ exp2 :mod)*.
+exp0 = exp1 (  '+'_  exp1 :add
+             | '-'_  exp1 :sub)*.
+exp1 = exp2 (  '*'_  exp2 :mul
+             | '//'_ exp2 :div
+             | '/'_  exp2 :truediv
+             | '%'_  exp2 :mod)*.
 exp2 = exp3 ('^'_ exp2 :pow)?.
 
 exp3 = '('_ exp0 ')'_
@@ -28,3 +29,7 @@ _    = /\s*/.
 #. (512,)
 ## g.top('5-3-1')
 #. (1,)
+## g.top('3//2')
+#. (1,)
+## g.top('3/2')
+#. (1.5,)
