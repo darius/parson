65379b01a82e306257e91256a9d328cc06d3e490
Darius Bacon <darius@wry.me>
2012-11-30 09:45:26 -0500

    add calculator example

---
+++ eg_calc.py
@@ -0,0 +1,30 @@
+"""
+The customary calculator example.
+"""
+
+from operator import *
+from parson import Grammar
+
+g = Grammar(r"""
+top  = _ exp0 ~/./.
+
+exp0 = exp1 (  '+'_ exp1 :add
+             | '-'_ exp1 :sub)*.
+exp1 = exp2 (  '*'_ exp2 :mul
+             | '/'_ exp2 :div
+             | '%'_ exp2 :mod)*.
+exp2 = exp3 ('^'_ exp2 :pow)?.
+
+exp3 = '('_ exp0 ')'_
+     | '-'_ exp1 :neg
+     | /(\d+)/_ :int.
+
+_    = /\s*/.
+""")(**globals())
+
+## g.top('42 * (5-3) + -2^2')
+#. (80,)
+## g.top('2^3^2')
+#. (512,)
+## g.top('5-3-1')
+#. (1,)
