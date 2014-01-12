f1f3576094aba538378382fc1d366ae2d8b90f42
Default User <default@example.com>
2014-01-12 00:06:59 -0300

    reorg for clarity

--- peg.py
+++ peg.py
@@ -11,35 +11,6 @@ to do: bounce less often
 +
 """
 
-# continuation: trail -> result
-# trail: () | ((vs,cs,ks), trail)
-# vs: tuple of values from semantic actions
-# cs: input character sequence (the current tail thereof)
-# ks: () | ((fail_cont,success_cont), ks)
-
-def Drop(cont): return lambda (_, trail): (cont, trail)
-def Nip(cont): return lambda (entry, (_, trail)): (cont, (entry, trail))
-def Dup(cont): return lambda (entry, trail): (cont, (entry, (entry, trail)))
-
-def fnsucceed(((vs,cs,((_,sk),ks)), trail)):
-    return sk, ((vs,cs,ks), trail)
-def fnfail(((vs,cs,((fk,_),ks)), trail)):
-    return fk, trail
-
-def finalfail(trail):
-    assert trail is ()
-    return None, 'fail'
-def finalsucceed(((vs,cs,ks), trail)):
-    assert trail is ()
-    assert ks is ()
-    return None, (vs, cs)
-
-def trampoline(cont, trail):
-    while cont is not None:
-        cont, trail = cont(trail)
-    return trail
-
-
 # peg constructors
 
 Fail = 'fail', None
@@ -55,41 +26,71 @@ def Cond(q, n, y): return 'cond', (q, n, y)
 
 def translate(pr, peg, f, s):
     tag, arg = peg
-    if tag == 'fail':
-        return Drop(f) 
-    elif tag == 'alter':
-        fn = arg
-        return lambda ((vs,cs,ks), trail): (s, ((fn(*vs),cs,ks), trail))
-    elif tag == 'item':
-        ok = arg
-        return lambda ((vs,cs,ks), trail): (
-            (s, ((vs+(cs[0],), cs[1:], ks), trail)) if cs and ok(cs[0])
-            else (f, trail))
-    elif tag == 'ref':
-        name = arg
-        return lambda ((vs,cs,ks), trail): (
-            pr[name], ((vs,cs,((f,s),ks)), trail))
-    elif tag == 'chain':
-        return translate(pr, arg[0], f,
-                         translate(pr, arg[1], f, s))
+    if tag == 'fail':    return KDrop(f) 
+    elif tag == 'alter': return KAlter(arg, s)
+    elif tag == 'item':  return KItem(arg, f, s)
+    elif tag == 'ref':   return KCall(pr, arg, f, s)
+    elif tag == 'chain': return translate(pr, arg[0], f,
+                                          translate(pr, arg[1], f, s))
     elif tag == 'cond':
         q, n, y = arg
         if y == q:
-            yy = Nip(s)
+            yy = KNip(s)
         elif y[0] == 'chain' and y[1][0] == q:
-            yy = Nip(translate(pr, y[1][1], f, s))
+            yy = KNip(translate(pr, y[1][1], f, s))
         else:
-            yy = Drop(translate(pr, y, f, s))
-        return Dup(translate(pr, q, translate(pr, n, f, s), yy))
+            yy = KDrop(translate(pr, y, f, s))
+        return KDup(translate(pr, q, translate(pr, n, f, s), yy))
     else:
         assert False
 
 def run(q, defns, vs, cs):
     pr = {}
     for name, defn in defns.items():
-        pr[name] = translate(pr, defn, fnfail, fnsucceed)
-    return trampoline(translate(pr, q, finalfail, finalsucceed),
+        pr[name] = translate(pr, defn, KFail, KSucceed)
+    return trampoline(translate(pr, q, KFinalFail, KFinalSucceed),
                       ((vs,cs,()), ()))
+
+
+# The parsing machine.
+# continuation: trail -> result
+# trail: () | ((vs,cs,ks), trail)
+# vs: tuple of values from semantic actions
+# cs: input character sequence (the current tail thereof)
+# ks: () | ((fail_cont,success_cont), ks)
+
+def trampoline(cont, trail):
+    while cont is not None:
+        cont, trail = cont(trail)
+    return trail
+
+def KFinalFail(trail):
+    assert trail is ()
+    return None, 'fail'
+def KFinalSucceed(((vs,cs,ks), trail)):
+    assert trail is ()
+    assert ks is ()
+    return None, (vs, cs)
+
+def KDrop(cont): return lambda (_, trail): (cont, trail)
+def KNip(cont): return lambda (entry, (_, trail)): (cont, (entry, trail))
+def KDup(cont): return lambda (entry, trail): (cont, (entry, (entry, trail)))
+
+def KAlter(fn, s):
+    return lambda ((vs,cs,ks), trail): (s, ((fn(*vs),cs,ks), trail))
+
+def KItem(ok, f, s):
+    return lambda ((vs,cs,ks), trail): (
+        (s, ((vs+(cs[0],), cs[1:], ks), trail)) if cs and ok(cs[0])
+        else (f, trail))
+
+def KCall(pr, name, f, s):
+    return lambda ((vs,cs,ks), trail): (
+        pr[name], ((vs,cs,((f,s),ks)), trail))
+def KFail(((vs,cs,((fk,_),ks)), trail)):
+    return fk, trail
+def KSucceed(((vs,cs,((_,sk),ks)), trail)):
+    return sk, ((vs,cs,ks), trail)
 
 
 # Smoke test
