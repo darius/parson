0b39e7a43b97923a08eef75ee936a4a7b7a5299b
Darius Bacon <darius@wry.me>
2014-12-18 21:39:47 -0800

    more-complete conversion

--- peglet_to_parson.py
+++ peglet_to_parson.py
@@ -1,29 +1,40 @@
 """
 Convert a Peglet grammar to a Parson one.
-Doesn't do everything: you need to change undefined names to :names in the output.
 """
 
-from parson import Grammar, join
+import re
+from parson import Grammar, alter
+
+name = r'[A-Za-z_]\w*'
 
 grammar = Grammar(r"""
-grammar ::= _? rule*              :join.
-rule    ::= id _ ['= ' :equ] token* [:dot] _?.
-token   ::= '|'                   :bar
+grammar ::= _? rule*.
+rule    ::= name _ '= ' :equ token* [:dot] _?.
+token   ::= '|'                     :bar
          |  /(\/\w*\/\s)/
-         |  id ~(_ '= ')
-         |  '!'                   :inv
-         |  _ ~(id _ '= ' | ~/./)
-         |  ~('= '|id) /(\S+)/    :regex.
-id      ::= /([A-Za-z_]\w*)/.
+         |  name ~(_ '= ')
+         |  '!'                     :inv
+         |  _ ~(name _ '= ' | ~/./)
+         |  ~('= '|name) /(\S+)/    :regex.
+name    ::= /("""+name+""")/.
 _       ::= /(\s+)/.
 """)
-parser = grammar(join  = join,
-                 equ   = lambda: '::= ',
-                 dot   = lambda: '.',
-                 bar   = lambda: '  | ',
-                 inv   = lambda: '~',
-                 regex = lambda s: '/'+s+'/')
+actions = dict(dot   = lambda: '.',
+               bar   = lambda: '  | ',
+               inv   = lambda: '~',
+               regex = lambda s: '/' + s.replace('/', '\\/') + '/')
+
+def peglet_to_parson(text):
+    nonterminals = set()
+    def equ(name, space):
+        nonterminals.add(name)
+        return name, space, '::= '
+    g = grammar(equ=alter(equ), **actions)
+    tokens = g.grammar(text)
+    return ''.join(':'+token if re.match(name+'$', token) and token not in nonterminals
+                   else token
+                   for token in tokens)
 
 if __name__ == '__main__':
     import sys
-    print parser.grammar(sys.stdin.read())[0]
+    print peglet_to_parson(sys.stdin.read())
