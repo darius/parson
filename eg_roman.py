1bfda2ecbc9f328c44adb39e65acd93ba22f18ef
Darius Bacon <darius@wry.me>
2016-06-07 13:29:50 -0400

    clearer layout, I guess

--- eg_roman.py
+++ eg_roman.py
@@ -6,13 +6,8 @@ from parson import Grammar
 
 g = Grammar(r"""
 numeral = digit+ :end.
-digit = 'CM' :'900' | 'M' :'1000'
-      | 'CD' :'400' | 'D'  :'500'
-      | 'XC'  :'90' | 'C'  :'100'
-      | 'XL'  :'40' | 'L'   :'50'
-      | 'IX'   :'9' | 'X'   :'10'
-      | 'IV'   :'4' | 'V'    :'5'
-                    | 'I'    :'1'.
+digit = 'CM'  :'900' | 'CD' :'400' | 'XC'  :'90' | 'XL' :'40' | 'IX'  :'9' | 'IV' :'4'
+      |  'M' :'1000' |  'D' :'500' |  'C' :'100' |  'L' :'50' |  'X' :'10' |  'V' :'5' | 'I' :'1'.
 """)()
 
 def int_from_roman(string):
