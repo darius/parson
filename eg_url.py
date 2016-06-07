9e62bfbf3cabbe23537a6be54cc709dab788c090
Darius Bacon <darius@wry.me>
2016-06-07 12:16:55 -0400

    add url example

---
+++ eg_url.py
@@ -0,0 +1,53 @@
+"""
+Based on https://www.w3.org/Addressing/URL/5_BNF.html
+because I'm a lazy bastard; it's clearly not up to date
+(as shown by 'right=wrong' below).
+"""
+
+from parson import Grammar
+
+grammar = r"""  url :end.
+
+url           : httpaddress | mailtoaddress.
+
+mailtoaddress : {'mailto'} ':' :'protocol' xalpha+ '@' hostname.
+
+httpaddress   : {'http'} '://' :'protocol' hostport ('/' path)? ('?' search)? ('#' fragment)?.
+
+hostport      : host (':' port)?.
+
+host          : {hostname | hostnumber} :'host'.
+hostname      : ialpha ('.' ialpha)*.
+hostnumber    : digits '.' digits '.' digits '.' digits.
+
+port          : {digits} :'port'.
+
+path          : {(segment '/')* segment?} :'path'.
+segment       : xpalpha+.
+
+search        : {xalpha+ ('+' xalpha+)*}  :'search'.
+fragment      : {xalpha+}                 :'fragment'.
+
+xalpha        : alpha | digit | safe | extra | escape.
+xpalpha       : xalpha | '+'.
+
+ialpha        : alpha xalpha*.
+
+alpha         : /[a-zA-Z]/.
+digit         : /\d/.
+digits        : /\d+/.
+safe          : /[$_@.&+-]/.
+extra         : /[!*"'(),]/.
+escape        : '%' hex hex.
+hex           : /[\dA-Fa-f]/.
+"""
+g = Grammar(grammar)()
+
+## g.attempt('true')
+## g('http://google.com')
+#. ('http', 'protocol', 'google.com', 'host')
+## g('http://en.wikipedia.org/wiki/Uniform_resource_locator')
+#. ('http', 'protocol', 'en.wikipedia.org', 'host', 'wiki/Uniform_resource_locator', 'path')
+## g.attempt('http://wry.me/fun/toys/yes.html?right=wrong#fraggle')
+## g(        'http://wry.me/fun/toys/yes.html?rightwrong#fraggle')
+#. ('http', 'protocol', 'wry.me', 'host', 'fun/toys/yes.html', 'path', 'rightwrong', 'search', 'fraggle', 'fragment')
