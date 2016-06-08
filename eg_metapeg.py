6c7a474a2fe2d47d08514757eb255a1aa713eee6
Darius Bacon <darius@wry.me>
2016-06-07 22:56:23 -0400

    add **,++ to metapeg

--- eg_metapeg.py
+++ eg_metapeg.py
@@ -22,7 +22,9 @@ pe           :  term ('|'_ pe :either)?
              |                                 :mk_empty.
 term         :  factor (term :chain)?.
 factor       :  '!'_ factor                    :invert
-             |  primary ('*'_ :star
+             |  primary ('**'_ primary :star
+                        |'++'_ primary :plus
+                        |'*'_ :star
                         |'+'_ :plus
                         |'?'_ :maybe)?.
 primary      :  '('_ pe ')'_
@@ -51,7 +53,7 @@ metapeg = Grammar(meta_grammar)(**globals())
 #. rule [('<name>' (((literal('=') ('<_>' '<pe>'))|(literal(':') ('<whitespace>' [('<pe>' :seclude)]))) (literal('.') ('<_>' :hug))))]
 #. pe [(('<term>' ((literal('|') ('<_>' ('<pe>' :either))))?)|:mk_empty)]
 #. term [('<factor>' (('<term>' :chain))?)]
-#. factor [((literal('!') ('<_>' ('<factor>' :invert)))|('<primary>' (((literal('*') ('<_>' :star))|((literal('+') ('<_>' :plus))|(literal('?') ('<_>' :maybe)))))?))]
+#. factor [((literal('!') ('<_>' ('<factor>' :invert)))|('<primary>' (((literal('**') ('<_>' ('<primary>' :star)))|((literal('++') ('<_>' ('<primary>' :plus)))|((literal('*') ('<_>' :star))|((literal('+') ('<_>' :plus))|(literal('?') ('<_>' :maybe)))))))?))]
 #. primary [((literal('(') ('<_>' ('<pe>' (literal(')') '<_>'))))|((literal('[') ('<_>' ('<pe>' (literal(']') ('<_>' :seclude)))))|((literal('{') ('<_>' ('<pe>' (literal('}') ('<_>' :capture)))))|(('<qstring>' :literal)|((literal('/') (('<regex_char>')* (literal('/') ('<_>' (:join :match)))))|((literal(':') (('<name>' :meta_mk_feed)|('<qstring>' :push)))|('<name>' :meta_mk_rule_ref)))))))]
 #. qstring [(/'/ (('<quoted_char>')* (/'/ ('<_>' :join))))]
 #. quoted_char [(/\\(.)/|/([^'])/)]
