5a818046882d83f54479e46ebf71e517e300114c
Darius Bacon <darius@wry.me>
2016-06-16 07:26:32 -0400

    fnordify

--- eg_calc_to_rpn.py
+++ eg_calc_to_rpn.py
@@ -4,21 +4,21 @@ Tiny example of 'compiling'.
 
 from parson import Grammar, alter
 
-g = Grammar(r"""
-top   :  _ stmt* :end.
+g = Grammar(r"""  stmt* :end.
 
-stmt  :  ident '='_ exp0 ';'_ :assign.
+stmt   :  ident '=' exp0 ';'  :assign.
 
-exp0  :  exp1 ('+'_ exp1      :'add')*.
-exp1  :  exp2 ('*'_ exp2      :'mul')*.
+exp0   :  exp1 ('+' exp1      :'add')*.
+exp1   :  exp2 ('*' exp2      :'mul')*.
 
-exp2  :  '('_ exp0 ')'_
-      |  /(\d+)/_
-      |  ident                :'fetch'.
+exp2   :  '(' exp0 ')'
+       |  /(\d+)/
+       |  ident               :'fetch'.
 
-ident :  /([A-Za-z]+)/_.
-_     :  /\s*/.
+ident  :  /([A-Za-z]+)/.
+
+FNORD ~:  /\s*/.
 """)(assign=alter(lambda name, *exp: exp + (name, 'store')))
 
-## print ' '.join(g.top('v = 42 * (5+3) + 2*2; v = v + 1;'))
+## print ' '.join(g('v = 42 * (5+3) + 2*2; v = v + 1;'))
 #. 42 5 3 add mul 2 2 mul add v store v fetch 1 add v store
