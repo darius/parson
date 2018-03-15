7c950631dc5de7bc257463d6f92717d479d3e39b
Darius Bacon <darius@wry.me>
2018-03-15 11:45:29 -0700

    parenthesize less, paying attention to context

--- eg_itsy/ast.py
+++ eg_itsy/ast.py
@@ -18,6 +18,9 @@ def embrace(lines):
 def opt_c(opt_x, if_none, if_some):
     return if_none if opt_x is None else if_some % opt_x.c()
 
+def opt_cexp(opt_x, if_none, if_some, p=0):
+    return if_none if opt_x is None else if_some % opt_x.c(p)
+
 
 # Declarations
 
@@ -25,19 +28,19 @@ class Let(Struct('names type opt_exp')):
     def c(self):
         if self.opt_exp is not None and len(self.names) != 1:
             raise Exception("XXX yadda yadda")
-        assign = opt_c(self.opt_exp, '', ' = %s')
+        assign = opt_cexp(self.opt_exp, '', ' = %s', list_context)
         return '\n'.join('%s%s;' % (self.type.c_decl(name), assign)
                          for name in self.names)
 
 class Array_decl(Struct('name type exps')):
     def c(self):
-        inits = [e.c() for e in self.exps]
-        return '%s = %s;' % (self.type.c_decl(self.name), embrace(inits))
+        return '%s = %s;' % (self.type.c_decl(self.name),
+                             embrace(e.c(list_context) for e in self.exps))
 
 class Enum(Struct('opt_name pairs')):
     def c(self):
         # XXX is this right when we mix explicit and implicit values?
-        enums = ['%s%s,' % (name, opt_c(opt_exp, '', ' = %s'))
+        enums = ['%s%s,' % (name, opt_cexp(opt_exp, '', ' = %s'))
                  for name, opt_exp in self.pairs]
         return 'enum %s%s' % (self.opt_name + ' ' if self.opt_name else '',
                               embrace(enums))
@@ -90,10 +93,10 @@ class Pointer(Struct('type', supertype=(Type,))):
 
 class Array(Struct('type size', supertype=(Type,))):
     def c(self):
-        return '%s[%s]' % (self.type.c(), self.size.c())
+        return '%s[%s]' % (self.type.c(), self.size.c(0))
     def decl(self, e):
         a, b = self.type.decl(e)
-        return a, '%s[%s]' % (b, self.size.c())
+        return a, '%s[%s]' % (b, self.size.c(0))
 
 class Function(Struct('param_types return_type', supertype=(Type,))):
     def c(self):
@@ -108,11 +111,11 @@ class Function(Struct('param_types return_type', supertype=(Type,))):
 
 class Exp(Struct('opt_exp')):
     def c(self):
-        return opt_c(self.opt_exp, ';', '%s;')
+        return opt_cexp(self.opt_exp, ';', '%s;')
 
 class Return(Struct('opt_exp')):
     def c(self):
-        return opt_c(self.opt_exp, 'return;', 'return %s;')
+        return opt_cexp(self.opt_exp, 'return;', 'return %s;')
 
 class Break(Struct('')):
     def c(self):
@@ -124,35 +127,35 @@ class Continue(Struct('')):
 
 class While(Struct('exp block')):
     def c(self):
-        return 'while (%s) %s' % (self.exp.c(), self.block.c())
+        return 'while (%s) %s' % (self.exp.c(0), self.block.c())
 
 class Do(Struct('block exp')):
     def c(self):
-        return 'do %s while(%s)' % (self.block.c(), self.exp.c())
+        return 'do %s while(%s)' % (self.block.c(), self.exp.c(0))
 
 class Ifs(Struct('parts')):
     def c(self):
         clauses = zip(self.parts[0:-1:2], self.parts[1::2])
         else_block = self.parts[-1]
-        ifs = ' else '.join('if (%s) %s' % (exp.c(), block.c())
+        ifs = ' else '.join('if (%s) %s' % (exp.c(0), block.c())
                             for exp, block in clauses)
         return ifs + opt_c(else_block, '', ' else %s')
 
 class For(Struct('opt_e1 opt_e2 opt_e3 block')):
     def c(self):
-        e1 = opt_c(self.opt_e1, '', '%s')
-        e2 = opt_c(self.opt_e2, '', '%s')
-        e3 = opt_c(self.opt_e3, '', '%s')
+        e1 = opt_cexp(self.opt_e1, '', '%s')
+        e2 = opt_cexp(self.opt_e2, '', '%s')
+        e3 = opt_cexp(self.opt_e3, '', '%s')
         return 'for (%s; %s; %s) %s' % (e1, e2, e3, self.block.c())
 
 class Switch(Struct('exp cases')):
     def c(self):
         cases = [case_.c() for case_ in self.cases]
-        return 'switch (%s) %s' % (self.exp.c(), embrace(cases))
+        return 'switch (%s) %s' % (self.exp.c(0), embrace(cases))
 
 class Case(Struct('exps block')):
     def c(self):
-        cases = '\n'.join('case %s:' % e.c() for e in self.exps)
+        cases = '\n'.join('case %s:' % e.c(0) for e in self.exps)
         return '%s %s\nbreak;' % (cases, self.block.c())
 
 class Default(Struct('block')):
@@ -167,101 +170,153 @@ class Block(Struct('decls stmts')):
 # Expressions
 
 class Literal(Struct('value')):
-    def c(self):
+    def c(self, p):
         return repr(self.value)
 
 class String(Struct('value')):
-    def c(self):
+    def c(self, p):
         return '"%s"' % self.value       # XXX escaping as C
 
 class Char_literal(Struct('value')):
-    def c(self):
+    def c(self, p):
         return "'%s'" % self.value    # XXX escaping
 
 class Variable(Struct('name')):
-    def c(self):
+    def c(self, p):
         return self.name
 
-class Address_of(Struct('exp')):
-    def c(self):
-        return '(&%s)' % self.exp.c()
+class Address_of(Struct('e1')):
+    def c(self, p):
+        return fmt1(p, unary_prec, '&%s', self.e1)
 
 class Sizeof_type(Struct('type')):
-    def c(self):
+    def c(self, p):
         return 'sizeof(%s)' % self.type.c()
 
-class Sizeof(Struct('exp')):
-    def c(self):
-        return '(sizeof %s)' % self.exp.c()
+class Sizeof(Struct('e1')):
+    def c(self, p):
+        return fmt1(p, unary_prec, 'sizeof %s', self.e1)
 
-class Deref(Struct('exp')):
-    def c(self):
-        return '(*%s)' % self.exp.c()
+class Deref(Struct('e1')):
+    def c(self, p):
+        return fmt1(p, unary_prec, '*%s', self.e1)
+
+class Unary_exp(Struct('unop e1')):
+    def c(self, p):
+        return fmt1(p, unary_prec, self.unop + '%s', self.e1)
 
 class Cast(Struct('type exp')):
-    def c(self):
-        return '((%s) %s)' % (self.type.c(), self.exp.c())
+    def c(self, p):
+        return wrap(cast_prec, p, '(%s) %s' % (self.type.c(), self.exp.c(cast_prec)))
 
 class Seq(Struct('e1 e2')):
-    def c(self):
-        return '(%s, %s)' % (self.e1.c(), self.e2.c())
+    def c(self, p):
+        return fmt2(p, ',', self.e1, self.e2, fmt_str = '%s%s %s')
 
-class Pre_incr(Struct('exp')):
-    def c(self):
-        return '(++%s)' % self.exp.c()
+class Pre_incr(Struct('e1')):
+    def c(self, p):
+        return fmt1(p, unary_prec, '++%s', self.e1)
 
-class Pre_decr(Struct('exp')):
-    def c(self):
-        return '(--%s)' % self.exp.c()
+class Pre_decr(Struct('e1')):
+    def c(self, p):
+        return fmt1(p, unary_prec, '--%s', self.e1)
 
-class Post_incr(Struct('exp')):
-    def c(self):
-        return '(%s++)' % self.exp.c()
+class Post_incr(Struct('e1')):
+    def c(self, p):
+        return fmt1(p, postfix_prec, '%s++', self.e1)
 
-class Post_decr(Struct('exp')):
-    def c(self):
-        return '(%s--)' % self.exp.c()
+class Post_decr(Struct('e1')):
+    def c(self, p):
+        return fmt1(p, postfix_prec, '%s--', self.e1)
 
 def Ifs_exp(e1, e2, e3, *es):
     return If_exp(e1, e2, e3 if not es else Ifs_exp(e3, *es))
 
 class If_exp(Struct('e1 e2 e3')):
-    def c(self):
-        return '(%s ? %s : %s)' % (self.e1.c(), self.e2.c(), self.e3.c())
+    def c(self, p):
+        lp, rp = binaries['?:']
+        return wrap(rp, p, # TODO recheck that rp is the right thing here in place of the usual lp
+                    '%s ? %s : %s' % (self.e1.c(lp),
+                                      self.e2.c(0),
+                                      self.e3.c(rp)))
 
 class Assign(Struct('e1 opt_binop e2')):
-    def c(self):
-        op = self.opt_binop or ''
-        return '(%s %s= %s)' % (self.e1.c(), op, self.e2.c())
+    def c(self, p):
+        return fmt2(p, (self.opt_binop or '') + '=', self.e1, self.e2) # TODO clumsy
 
 class Binary_exp(Struct('e1 binop e2')):
-    def c(self):
-        return '(%s %s %s)' % (self.e1.c(), self.binop, self.e2.c())
-
-class Unary_exp(Struct('unop e1')):
-    def c(self):
-        return '(%s%s)' % (self.unop, self.e1.c())
+    def c(self, p):
+        return fmt2(p, self.binop, self.e1, self.e2)
 
 class Index(Struct('e1 e2')):
-    def c(self):
-        return '%s[%s]' % (self.e1.c(), self.e2.c())
+    def c(self, p):
+        return wrap(postfix_prec, p,
+                    '%s[%s]' % (self.e1.c(postfix_prec), self.e2.c(0)))
 
 class Call(Struct('e1 args')):
-    def c(self):
-        # TODO: beware of precedence problems from assuming ', ' precedence
-        return '%s(%s)' % (self.e1.c(), ', '.join(e.c() for e in self.args))
+    def c(self, p):
+        return wrap(postfix_prec, p,
+                    '%s(%s)' % (self.e1.c(postfix_prec),
+                                ', '.join(e.c(list_context) for e in self.args)))
 
 class Dot(Struct('e1 field')):
-    def c(self):
+    def c(self, p):
         if isinstance(self.e1, Deref):
-            return '%s->%s' % (self.e1.exp.c(), self.field)
+            s = '%s->%s' % (self.e1.exp.c(postfix_prec), self.field)
         else:
-            return '%s.%s' % (self.e1.c(), self.field)
+            s = '%s.%s' % (self.e1.c(postfix_prec), self.field)
+        return wrap(postfix_prec, p, s)
 
 class And(Struct('e1 e2')):
-    def c(self):
-        return '(%s && %s)' % (self.e1.c(), self.e2.c())
+    def c(self, p):
+        return fmt2(p, '&&', self.e1, self.e2)
 
 class Or(Struct('e1 e2')):
-    def c(self):
-        return '(%s || %s)' % (self.e1.c(), self.e2.c())
+    def c(self, p):
+        return fmt2(p, '||', self.e1, self.e2)
+
+
+# Parenthesizing by precedence
+
+infix_precedence_tower = """\
+,
+=
+?:
+||
+&&
+|
+^
+&
+== !=
+< > <= >=
+<< >>
++ -
+* / %
+(cast)
+(unary)
+(postfix)""".splitlines()
+
+binaries = {op: (2*i, 2*i+1)
+            for i, line in enumerate(infix_precedence_tower)
+            for op in line.split()}
+cast_prec    = binaries['(cast)'][0]
+unary_prec   = binaries['(unary)'][0]
+postfix_prec = binaries['(postfix)'][0]
+
+list_context = binaries['='][0]  # The next precedence after ','
+
+# Make the left precedence of the assignment operator be unary_expression, and right-associative:
+binaries['='] = (unary_prec, binaries['='][0])
+
+# Also, ?: is also right-associative:
+binaries['?:'] = (binaries['?:'][0], binaries['?:'][0])
+
+def fmt1(outer, inner, fmt_str, e1):
+    return wrap(inner, outer, fmt_str % e1.c(inner))
+
+def fmt2(p, op, e1, e2, fmt_str='%s %s %s'):
+    lp, rp = binaries['=' if op.endswith('=') else op]
+    return wrap(lp, p, fmt_str % (e1.c(lp), op, e2.c(rp)))
+
+def wrap(inner, outer, s):
+    return '(%s)' % s if inner < outer else s
--- eg_itsy/itsy.py
+++ eg_itsy/itsy.py
@@ -10,20 +10,35 @@ with open('itsy.parson') as f:
 grammar = Grammar(grammar_source)
 parse = grammar.bind(ast)
 
-p1 = parse.top('let b: int[5];')[0]
+p1, = parse.top('let b: int[5];')
 ## p1.c()
 #. 'int b[5];'
 
-p2 = parse.top('let b: int[5]@;')[0]
+p2, = parse.top('let b: int[5]@;')
 ## p2.c()
 #. 'int (*b)[5];'
 
-p3 = parse.top('let b: int[8][1];')[0]
+p3, = parse.top('let b: int[8][1];')
 ## p3.c()
 #. 'int b[8][1];'
 
 ## parse.top('to f(): void {}')[0].c()
 #. 'void f(void) {\n  \n}'
+
+p4, = parse.top('let a: int = (1, 2, 3);')
+p4, = parse.top('let a: int = 1, 2, 3;')   # XXX ugh this syntax
+## p4.c()
+#. 'int a = (1, 2, 3);'
+
+p5, = parse.top('let a: int = a@++@;')
+## p5.c()
+#. 'int a = *(*a)++;'
+
+# I guess this output without parens is actually correct, though confusing to read. TODO check
+# Maybe we should just always parenthesize a run of ',' operators...
+p6, = parse.statement('return if pattern < pp && pp[-1] == c {--pp, 1} else {0};')
+## p6.c()
+#. 'return pattern < pp && pp[-1] == c ? --pp, 1 : 0;'
 
 with open('itsy.examples') as f:
     examples = f.read()
@@ -31,29 +46,29 @@ with open('itsy.examples') as f:
 #. int a = 5;
 #. 
 #. int f(int x) {
-#.   return (x * x);
+#.   return x * x;
 #. }
 #. 
 #. int fib(int n) {
-#.   return ((n < 2) ? 1 : (fib((n - 1)) + fib((n - 2))));
+#.   return n < 2 ? 1 : fib(n - 1) + fib(n - 2);
 #. }
 #. 
 #. int fact(int n) {
 #.   int p = 1;
-#.   for (; (0 < n); (--n)) {
-#.     (p *= n);
+#.   for (; 0 < n; --n) {
+#.     p *= n;
 #.   }
 #.   return p;
 #. }
 #. 
 #. void asm_store_field(Address address, int L, int R, Cell cell) {
-#.   assert((address < memory_size));
+#.   assert(address < memory_size);
 #.   if (VERBOSE) {
 #.     int temp[12];
 #.     unparse_cell(temp, cell);
 #.     printf("%4o(%u,%u): %s\n", address, L, R, temp);
 #.   }
-#.   (memory[address] = set_field(cell, make_field_spec(L, R), memory[address]));
+#.   memory[address] = set_field(cell, make_field_spec(L, R), memory[address]);
 #. }
 #. 
 
@@ -97,33 +112,33 @@ with open('regex.itsy') as f:
 #. };
 #. 
 #. void dump1(int pc) {
-#.   printf("%c %2u: %-4s ", (accepts[pc] ? '*' : ' '), pc, names[ops[pc]]);
-#.   printf(((pc == accept) ? "\n" : ((ops[pc] == op_eat) ? "'%c' %d\n" : "%d %d\n")), arg1[pc], arg2[pc]);
+#.   printf("%c %2u: %-4s ", accepts[pc] ? '*' : ' ', pc, names[ops[pc]]);
+#.   printf(pc == accept ? "\n" : ops[pc] == op_eat ? "'%c' %d\n" : "%d %d\n", arg1[pc], arg2[pc]);
 #. }
 #. 
 #. uint8 occupied[max_insns];
 #. 
 #. void after(char ch, int start, int end, int (*(*next_states))) {
-#.   while ((start != end)) {
+#.   while (start != end) {
 #.     int r = arg1[start];
 #.     int s = arg2[start];
 #.     switch (ops[start]) {
 #.       case op_eat: {
-#.         if (((r == ch) && (!occupied[s]))) {
-#.           ((*((*next_states)++)) = s);
-#.           (occupied[s] = 1);
+#.         if (r == ch && !occupied[s]) {
+#.           *(*next_states)++ = s;
+#.           occupied[s] = 1;
 #.         }
 #.         return;
 #.       }
 #.       break;
 #.       case op_fork: {
 #.         after(ch, r, end, next_states);
-#.         (start = s);
+#.         start = s;
 #.       }
 #.       break;
 #.       case op_loop: {
 #.         after(ch, r, start, next_states);
-#.         (start = s);
+#.         start = s;
 #.       }
 #.       break;
 #.       default: {
@@ -145,71 +160,71 @@ with open('regex.itsy') as f:
 #.   if (accepts[start]) {
 #.     return 1;
 #.   }
-#.   ((cur_start = states0), (cur_end = cur_start));
-#.   ((next_start = states1), (next_end = next_start));
-#.   ((*(cur_end++)) = start);
+#.   cur_start = states0, cur_end = cur_start;
+#.   next_start = states1, next_end = next_start;
+#.   *cur_end++ = start;
 #.   memset(occupied, 0, ninsns);
-#.   for (; (*input); (++input)) {
+#.   for (; *input; ++input) {
 #.     int (*state);
-#.     for ((state = cur_start); (state < cur_end); (++state)) {
-#.       after((*input), (*state), accept, (&next_end));
+#.     for (state = cur_start; state < cur_end; ++state) {
+#.       after(*input, *state, accept, &next_end);
 #.     }
-#.     for ((state = next_start); (state < next_end); (++state)) {
-#.       if (accepts[(*state)]) {
+#.     for (state = next_start; state < next_end; ++state) {
+#.       if (accepts[*state]) {
 #.         return 1;
 #.       }
-#.       (occupied[(*state)] = 0);
+#.       occupied[*state] = 0;
 #.     }
 #.     {
 #.       int (*t) = cur_start;
-#.       ((cur_start = next_start), (cur_end = next_end));
-#.       (next_start = (next_end = t));
+#.       cur_start = next_start, cur_end = next_end;
+#.       next_start = next_end = t;
 #.     }
 #.   }
 #.   return 0;
 #. }
 #. 
 #. int emit(uint8 op, int r, int s, uint8 accepting) {
-#.   if ((max_insns <= ninsns)) {
+#.   if (max_insns <= ninsns) {
 #.     error("Pattern too long");
 #.   }
-#.   ((ops[ninsns] = op), ((arg1[ninsns] = r), (arg2[ninsns] = s)));
-#.   (accepts[ninsns] = accepting);
-#.   return (ninsns++);
+#.   ops[ninsns] = op, arg1[ninsns] = r, arg2[ninsns] = s;
+#.   accepts[ninsns] = accepting;
+#.   return ninsns++;
 #. }
 #. 
 #. char (*pattern);
 #. char (*pp);
 #. 
 #. int eat(char c) {
-#.   return (((pattern < pp) && (pp[(-1)] == c)) ? ((--pp), 1) : 0);
+#.   return pattern < pp && pp[-1] == c ? --pp, 1 : 0;
 #. }
 #. 
 #. int parsing(int precedence, int state) {
 #.   int rhs;
-#.   if (((pattern == pp) || ((pp[(-1)] == '(') || (pp[(-1)] == '|')))) {
-#.     (rhs = state);
+#.   if (pattern == pp || (pp[-1] == '(' || pp[-1] == '|')) {
+#.     rhs = state;
 #.   } else if (eat(')')) {
-#.     (rhs = parsing(0, state));
-#.     if ((!eat('('))) {
+#.     rhs = parsing(0, state);
+#.     if (!eat('(')) {
 #.       error("Mismatched ')'");
 #.     }
 #.   } else if (eat('*')) {
-#.     (rhs = emit(op_loop, 0, state, accepts[state]));
-#.     (arg1[rhs] = parsing(6, rhs));
+#.     rhs = emit(op_loop, 0, state, accepts[state]);
+#.     arg1[rhs] = parsing(6, rhs);
 #.   } else {
-#.     (rhs = emit(op_eat, (*(--pp)), state, 0));
+#.     rhs = emit(op_eat, *--pp, state, 0);
 #.   }
-#.   while (((pattern < pp) && (pp[(-1)] != '('))) {
-#.     int prec = ((pp[(-1)] == '|') ? 3 : 5);
-#.     if ((prec <= precedence)) {
+#.   while (pattern < pp && pp[-1] != '(') {
+#.     int prec = pp[-1] == '|' ? 3 : 5;
+#.     if (prec <= precedence) {
 #.       break;
 #.     }
 #.     if (eat('|')) {
 #.       int rhs2 = parsing(prec, state);
-#.       (rhs = emit(op_fork, rhs, rhs2, (accepts[rhs] || accepts[rhs2])));
+#.       rhs = emit(op_fork, rhs, rhs2, accepts[rhs] || accepts[rhs2]);
 #.     } else {
-#.       (rhs = parsing(prec, rhs));
+#.       rhs = parsing(prec, rhs);
 #.     }
 #.   }
 #.   return rhs;
@@ -217,11 +232,11 @@ with open('regex.itsy') as f:
 #. 
 #. int parse(char (*string)) {
 #.   int state;
-#.   (pattern = string);
-#.   (pp = (pattern + strlen(pattern)));
-#.   (ninsns = 0);
-#.   (state = parsing(0, emit(op_accept, 0, 0, 1)));
-#.   if ((pattern != pp)) {
+#.   pattern = string;
+#.   pp = pattern + strlen(pattern);
+#.   ninsns = 0;
+#.   state = parsing(0, emit(op_accept, 0, 0, 1));
+#.   if (pattern != pp) {
 #.     error("Bad pattern");
 #.   }
 #.   return state;
@@ -231,20 +246,20 @@ with open('regex.itsy') as f:
 #.   char line[9999];
 #.   int matched = 0;
 #.   int start_state;
-#.   if ((argc != 2)) {
+#.   if (argc != 2) {
 #.     error("Usage: grep pattern");
 #.   }
-#.   (start_state = parse(argv[1]));
+#.   start_state = parse(argv[1]);
 #.   if (loud) {
 #.     printf("start: %u\n", start_state);
 #.     dump();
 #.   }
-#.   while (fgets(line, (sizeof line), stdin)) {
-#.     (line[(strlen(line) - 1)] = '\0');
-#.     if ((matched |= run(start_state, line))) {
+#.   while (fgets(line, sizeof line, stdin)) {
+#.     line[strlen(line) - 1] = '\0';
+#.     if (matched |= run(start_state, line)) {
 #.       puts(line);
 #.     }
 #.   }
-#.   return (matched ? 0 : 1);
+#.   return matched ? 0 : 1;
 #. }
 #. 
