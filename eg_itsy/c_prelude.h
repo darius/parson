f447f9efbb63f329edbe0b82110c556ec558ea00
Darius Bacon <darius@wry.me>
2018-03-16 22:30:23 -0700

    add missing bits

--- eg_itsy/c_prelude.h
+++ eg_itsy/c_prelude.h
@@ -1,7 +1,11 @@
 // Standard prelude for Itsy code translated to C.
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // TODO more things like uint8, etc.
+typedef unsigned char uint8;
+
 
 // End of prelude.
