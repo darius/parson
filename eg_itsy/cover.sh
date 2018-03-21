19f5cd963256ef524b230e1b47e7305a4d99d606
Darius Bacon <darius@wry.me>
2018-03-21 12:35:08 -0700

    add test cases for errors

--- eg_itsy/cover.sh
+++ eg_itsy/cover.sh
@@ -4,6 +4,7 @@ python -m coverage erase
 
 python -m coverage run -a itsy.py goober
 python -m coverage run -a itsy.py eg/bad.itsy 
+python -m coverage run -a itsy.py eg/bad2.itsy 
 
 python -m coverage run -a itsy.py eg/examples.itsy 
 python -m coverage run -a itsy.py eg/ion.itsy 
