dbf2dbcbd063e647da6aef77761d79d0204be55f
Darius Bacon <darius@wry.me>
2018-03-21 11:27:28 -0700

    add coverage checking

---
+++ eg_itsy/cover.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+python -m coverage erase
+
+python -m coverage run -a itsy.py goober
+python -m coverage run -a itsy.py eg/bad.itsy 
+
+python -m coverage run -a itsy.py eg/examples.itsy 
+python -m coverage run -a itsy.py eg/ion.itsy 
+python -m coverage run -a itsy.py eg/regex.itsy 
+python -m coverage run -a itsy.py eg/superopt.itsy
+
+python -m coverage report -m | grep -v /parson
