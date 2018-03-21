d56fd3df1fb7bf7f6dcc4feab77b388bc0920cc0
Darius Bacon <darius@wry.me>
2018-03-21 10:51:32 -0700

    tweak

--- eg_itsy/itsy.py
+++ eg_itsy/itsy.py
@@ -15,6 +15,11 @@ parser = Grammar(grammar_source).bind(ast)
 with open('c_prelude.h') as f:
     c_prelude = f.read()
 
+
+def main(argv):
+    assert 2 <= len(argv), "usage: %s source_file.itsy [output_c_file.c]" % argv[0]
+    return to_c_main(*argv[1:])
+
 def to_c_main(filename, out_filename=None):
     if out_filename is None:
         if not filename.endswith('.itsy'):
@@ -23,7 +28,7 @@ def to_c_main(filename, out_filename=None):
     with open(filename) as f:
         source = f.read()
     try:
-        c = c_from_itsy(source, filename)
+        c = c_from_itsy(source)
     except Unparsable as exc:
         sys.stderr.write(syntax_error(exc, filename) + '\n')
         return 1
@@ -32,12 +37,11 @@ def to_c_main(filename, out_filename=None):
         f.write(c)
     return 0
 
-def c_from_itsy(source, filename=''):
+def c_from_itsy(source):
     defs = parser.top(source)
     c_defs = map(c_emit, defs)
     return c_prelude + '\n' + '\n\n'.join(c_defs) + '\n'
 
 
 if __name__ == '__main__':
-    assert 2 <= len(sys.argv) <= 3, "usage: itsy.py source_file.itsy [output_c_file.c]"
-    sys.exit(to_c_main(*sys.argv[1:]))
+    sys.exit(main(sys.argv))
