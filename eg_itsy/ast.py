e0e84e58bb86cb6b31080edbb72d699225bc7f6d
Darius Bacon <darius@wry.me>
2018-03-22 16:55:06 -0700

    add position info to ast (rather ugly)

--- eg_itsy/ast.py
+++ eg_itsy/ast.py
@@ -7,41 +7,41 @@ from structs import Struct
 
 # Declarations
 
-class To(        Struct('name signature body')): pass
-class Typedef(   Struct('name type')): pass
-class Let(       Struct('names type opt_exp')): pass
-class Enum(      Struct('opt_name pairs')): pass
-class Record(    Struct('kind opt_name fields')): pass # kind = 'struct' or 'union'
+class To(        Struct('pos name signature body')): pass
+class Typedef(   Struct('pos name type')): pass
+class Let(       Struct('pos names type opt_exp')): pass
+class Enum(      Struct('pos opt_name pairs')): pass
+class Record(    Struct('pos kind opt_name fields')): pass # kind = 'struct' or 'union'
 
 
 # Statements
 
-class Block(     Struct('parts')): pass
+class Block(     Struct('pos parts')): pass
 
-class Exp(       Struct('opt_exp')): pass
-class Return(    Struct('opt_exp')): pass
-class Break(     Struct('')): pass
-class Continue(  Struct('')): pass
-class While(     Struct('exp block')): pass
-class Do(        Struct('block exp')): pass
-class Ifs(       Struct('parts')): pass
-class For(       Struct('opt_e1 opt_e2 opt_e3 block')): pass
-class Switch(    Struct('exp cases')): pass
+class Exp(       Struct('pos opt_exp')): pass
+class Return(    Struct('pos opt_exp')): pass
+class Break(     Struct('pos ')): pass
+class Continue(  Struct('pos ')): pass
+class While(     Struct('pos exp block')): pass
+class Do(        Struct('pos block exp')): pass
+class Ifs(       Struct('pos parts')): pass
+class For(       Struct('pos opt_e1 opt_e2 opt_e3 block')): pass
+class Switch(    Struct('pos exp cases')): pass
 
-class Case(      Struct('exps block')): pass
-class Default(   Struct('block')): pass
+class Case(      Struct('pos exps block')): pass
+class Default(   Struct('pos block')): pass
 
 
 # Types
 
-class Type_name( Struct('name')): pass
-class Pointer(   Struct('type')): pass
-class Array(     Struct('size type')): pass
-class Signature( Struct('params return_type')): pass  # params are (type, (name or '')) pairs
+class Type_name( Struct('pos name')): pass
+class Pointer(   Struct('pos type')): pass
+class Array(     Struct('pos size type')): pass
+class Signature( Struct('pos params return_type')): pass  # params are (type, (name or '')) pairs
 
 # TODO rename fields like 'type' to 'base_type' or something
 
-def Void(): return Type_name('void')  # for now, anyway
+def Void(pos): return Type_name(pos, 'void')  # for now, anyway
 
 def spread_params(names, type_):
     return tuple((type_, name) for name in names)
@@ -52,26 +52,28 @@ def chain(*seqs):
 
 # Expressions
 
-def Ifs_exp(e1, e2, e3, *es):
-    return If_exp(e1, e2, e3 if not es else Ifs_exp(e3, *es))
+def Ifs_exp(pos, e1, e2, arg, *rest):
+    else_ = arg if not rest else Ifs_exp(arg, *rest)
+    return If_exp(pos, e1, e2, else_)
 
-class Literal(     Struct('text kind')): pass
-class Variable(    Struct('name')): pass
-class Address_of(  Struct('e1')): pass
-class Sizeof_type( Struct('type')): pass
-class Sizeof(      Struct('e1')): pass
-class Deref(       Struct('e1')): pass
-class Unary_exp(   Struct('unop e1')): pass
-class Cast(        Struct('e1 type')): pass
 class Seq(         Struct('e1 e2')): pass
-class Pre_incr(    Struct('e1 op')): pass
-class Post_incr(   Struct('e1 op')): pass
-class If_exp(      Struct('e1 e2 e3')): pass
 class Assign(      Struct('e1 opt_binop e2')): pass
+class And(         Struct('e1 e2')): pass
+class Or(          Struct('e1 e2')): pass
 class Binary_exp(  Struct('e1 binop e2')): pass
 class Index(       Struct('e1 e2')): pass
 class Call(        Struct('e1 args')): pass
 class Dot(         Struct('e1 field')): pass
-class And(         Struct('e1 e2')): pass
-class Or(          Struct('e1 e2')): pass
-class Compound_exp(Struct('exps')): pass
+class Deref(       Struct('e1')): pass
+class Post_incr(   Struct('e1 op')): pass
+class Cast(        Struct('e1 type')): pass
+
+class Literal(     Struct('pos text kind')): pass
+class Variable(    Struct('pos name')): pass
+class Address_of(  Struct('pos e1')): pass
+class Sizeof_type( Struct('pos type')): pass
+class Sizeof(      Struct('pos e1')): pass
+class Unary_exp(   Struct('pos unop e1')): pass
+class Pre_incr(    Struct('pos e1 op')): pass
+class If_exp(      Struct('pos e1 e2 e3')): pass
+class Compound_exp(Struct('pos exps')): pass
